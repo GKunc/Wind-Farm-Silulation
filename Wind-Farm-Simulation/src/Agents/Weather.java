27d16cea71b618f58f6b1627575a253e68b6334d
Grzegorz Kunc <gkunc@kent.edu>
2019-04-26 21:27:56 +0200

    Chaning API for downloading weather

--- Wind-Farm-Simulation/src/Agents/Weather.java
+++ Wind-Farm-Simulation/src/Agents/Weather.java
@@ -83,91 +83,34 @@ public class Weather {
 
     public static Weather downloadWeather(String city) throws Exception { // pogoda z jednegodnia
 
-        final String appId = "nDC9AG4e";
-        final String consumerKey = "dj0yJmk9RXQyMXI0RmNSU2pqJnM9Y29uc3VtZXJzZWNyZXQmc3Y9MCZ4PTRl";
-        final String consumerSecret = "bc45a4094dd5b093b47b6fa167266c52740e0b1e";
-        final String url = "https://weather-ydn-yql.media.yahoo.com/forecastrss";
+        final String URL = "http://api.worldweatheronline.com/premium/v1/past-weather.ashx";
+        final String key = "535406b1d5e648e2b28183352192604";
 
         Double _wind = 0.0;
         Double _pressure = 0.0;
         Double _temperature = 0.0;
 
-        long timestamp = new Date().getTime() / 1000;
-        byte[] nonce = new byte[32];
-        Random rand = new Random();
-        rand.nextBytes(nonce);
-        String oauthNonce = new String(nonce).replaceAll("\\W", "");
-
-        List<String> parameters = new ArrayList<>();
-        parameters.add("oauth_consumer_key=" + consumerKey);
-        parameters.add("oauth_nonce=" + oauthNonce);
-        parameters.add("oauth_signature_method=HMAC-SHA1");
-        parameters.add("oauth_timestamp=" + timestamp);
-        parameters.add("oauth_version=1.0");
-        // Make sure value is encoded
-        parameters.add("location=" + URLEncoder.encode(city, "UTF-8"));
-        parameters.add("format=json");
-        parameters.add("u=c");
-        Collections.sort(parameters);
-
-        StringBuffer parametersList = new StringBuffer();
-        for (int i = 0; i < parameters.size(); i++) {
-            parametersList.append(((i > 0) ? "&" : "") + parameters.get(i));
-        }
-
-        String signatureString = "GET&" +
-                URLEncoder.encode(url, "UTF-8") + "&" +
-                URLEncoder.encode(parametersList.toString(), "UTF-8");
-
-        String signature = null;
-        try {
-            SecretKeySpec signingKey = new SecretKeySpec((consumerSecret + "&").getBytes(), "HmacSHA1");
-            Mac mac = Mac.getInstance("HmacSHA1");
-            mac.init(signingKey);
-            byte[] rawHMAC = mac.doFinal(signatureString.getBytes());
-            Encoder encoder = Base64.getEncoder();
-            signature = encoder.encodeToString(rawHMAC);
-        } catch (Exception e) {
-            System.err.println("Unable to append signature");
-            System.exit(0);
-        }
-
-        String authorizationLine = "OAuth " +
-                "oauth_consumer_key=\"" + consumerKey + "\", " +
-                "oauth_nonce=\"" + oauthNonce + "\", " +
-                "oauth_timestamp=\"" + timestamp + "\", " +
-                "oauth_signature_method=\"HMAC-SHA1\", " +
-                "oauth_signature=\"" + signature + "\", " +
-                "oauth_version=\"1.0\"";
-
         HttpClient client = HttpClient.newHttpClient(); // sunnyvale,ca
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create(url + "?location=" + city + "&format=json&u=c"))
-                .header("Authorization", authorizationLine)
-                .header("X-Yahoo-App-Id", appId)
-                .header("Content-Type", "application/json")
+                .uri(URI.create(URL + "?key=" + key
+                        + "&q=" + city
+                        + "&format=json"
+                        + "&date=2018-01-01"
+                        + "&enddate=2019-01-10"))
                 .build();
 
         HttpResponse<String> response = client.send(request, BodyHandler.asString());
         String result = response.body();
         String data[] = result.split("\\{");
 
-        for(int i = 0; i < data.length; ++i) {
-            if(data[i].contains("wind")) {
-                String[] info = data[i+1].split(",");
-                String str_w = info[2].split(":")[1];
-                String detail_wind = str_w.substring(0, str_w.length() - 1);
-                _wind = Double.parseDouble(detail_wind);
-
-                String detail_temperature = info[0].split(":")[1];
-                _temperature = Double.parseDouble(detail_temperature);
-            }
-            else if(data[i].contains("pressure")) {
-                String[] pressure_info = data[i].split(",");
-                String detail = pressure_info[2].split(":")[1];
-                _pressure = Double.parseDouble(detail);
-            }
-        }
+        /*
+        todo
+        sparsowanie wynikow
+        wybranie wiatru, cisnienia i temperatury
+        wstawienie do listy pogody
+        zmienienie zwracanego typu
+         */
+        for(String s : data) System.out.println(s);
         //System.out.println("DANE -> " + _wind + _pressure + _temperature);
         return new Weather(_wind, _pressure, _temperature);
     }
@@ -232,9 +175,9 @@ public class Weather {
 
    public static void main(String args[]) throws Exception {
        Weather w = new Weather();
-       w = Weather.downloadWeather("Gdansk");
+       w = Weather.downloadWeather("Gdańsk");
        //ArrayList<Weather> weathers = w.parseWeatherFromFile("./res/weatherGdansk.csv");
-       System.out.println(w);
+       //System.out.println(w);
 
    }
 }
