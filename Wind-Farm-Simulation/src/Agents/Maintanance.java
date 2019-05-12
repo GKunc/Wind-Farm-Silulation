5cc113421408fcd0b2100d572cb5f03596fe93b6
Grzegorz Kunc <grzeg.kunc@gmail.com>
2019-05-12 18:14:29 +0200

    Done annualMaintanance, before testes

--- Wind-Farm-Simulation/src/Agents/Maintanance.java
+++ Wind-Farm-Simulation/src/Agents/Maintanance.java
@@ -11,31 +11,30 @@ public class Maintanance {
      */
 
     public void preventiveMaintanance(Turbine turbine, int daysPassed) { // polroczny lub coroczny przeglad
-        if(daysPassed % 365 == 0 || turbine.annualMaintanance != 0) {
+        if(daysPassed % 365 == 0 || turbine.getAnnualMaintanance() != 0) {
             Maintanance.annualMaintanance(turbine);
         }
-        else if((daysPassed % 180 == 0 && daysPassed % 360 != 0) || turbine.halfYearMaintanance != 0) {
+        else if((daysPassed % 180 == 0 && daysPassed % 360 != 0) || turbine.getHalfYearMaintanance() != 0) {
             Maintanance.halfYearMaintanance(turbine);
-
         }
     }
 
     public static void annualMaintanance(Turbine turbine) {
-        if(turbine.annualMaintanance > 24) {
-            turbine.annualMaintanance = 0;
+        if(turbine.getAnnualMaintanance() > 24) {
+            turbine.setAnnualMaintanance(0);
             turbine.turnOn();
         } else {
-            turbine.annualMaintanance++;
+            turbine.setAnnualMaintanance(turbine.getAnnualMaintanance() + 1);
             turbine.turnOff();
         }
     }
 
     public static void halfYearMaintanance(Turbine turbine) {
-        if(turbine.halfYearMaintanance > 8) {
-            turbine.halfYearMaintanance = 0;
+        if(turbine.getHalfYearMaintanance() > 8) {
+            turbine.setHalfYearMaintanance(0);
             turbine.turnOn();
         } else {
-            turbine.annualMaintanance++;
+            turbine.setHalfYearMaintanance(turbine.getHalfYearMaintanance() + 1);
             turbine.turnOff();
         }
     }
