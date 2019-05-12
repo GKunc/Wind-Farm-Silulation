434ab925a98f21d28a882cfc1fbb2f1fec5a780c
Grzegorz Kunc <grzeg.kunc@gmail.com>
2019-05-12 17:59:47 +0200

    Done annual and half year maintanance functions

--- Wind-Farm-Simulation/src/Agents/Maintanance.java
+++ Wind-Farm-Simulation/src/Agents/Maintanance.java
@@ -11,18 +11,33 @@ public class Maintanance {
      */
 
     public void preventiveMaintanance(Turbine turbine, int daysPassed) { // polroczny lub coroczny przeglad
-        if(daysPassed % 365 == 0)
+        if(daysPassed % 365 == 0 || turbine.annualMaintanance != 0) {
             Maintanance.annualMaintanance(turbine);
-        else if(daysPassed % 180 == 0 && daysPassed % 360 != 0)
+        }
+        else if((daysPassed % 180 == 0 && daysPassed % 360 != 0) || turbine.halfYearMaintanance != 0) {
             Maintanance.halfYearMaintanance(turbine);
+
+        }
     }
 
     public static void annualMaintanance(Turbine turbine) {
-        
+        if(turbine.annualMaintanance > 24) {
+            turbine.annualMaintanance = 0;
+            turbine.turnOn();
+        } else {
+            turbine.annualMaintanance++;
+            turbine.turnOff();
+        }
     }
 
     public static void halfYearMaintanance(Turbine turbine) {
-
+        if(turbine.halfYearMaintanance > 8) {
+            turbine.halfYearMaintanance = 0;
+            turbine.turnOn();
+        } else {
+            turbine.annualMaintanance++;
+            turbine.turnOff();
+        }
     }
 
     public void alertMaintanance() { // naprawa jakiejs awarii
